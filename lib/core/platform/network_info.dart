38d760b2b9adfed66d2e8b4b044ac798c2dd7adc
Lê Anh Tài <leanhtaihcmus@gmail.com>
2022-10-11 17:39:56 +0700

    Implement TDD for NetWorkInfo and Mocks

--- lib/core/platform/network_info.dart
+++ lib/core/platform/network_info.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: unused_import, depend_on_referenced_packages
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
@@ -8,4 +9,13 @@ import 'network_info.mocks.dart';
 
 abstract class NetworkInfo {
   Future<bool> get isConnected;
+}
+
+class NetworkInfoImpl implements NetworkInfo {
+  final InternetConnectionChecker internetConnectionChecker;
+
+  const NetworkInfoImpl(this.internetConnectionChecker);
+
+  @override
+  Future<bool> get isConnected => internetConnectionChecker.hasConnection;
 }
---
+++ test/core/platform/network_info_test.dart
@@ -0,0 +1,36 @@
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:number_trivia/core/platform/network_info.dart';
+
+import 'network_info_test.mocks.dart';
+
+@GenerateMocks([InternetConnectionChecker])
+void main() {
+  late NetworkInfoImpl networkInfo;
+  late MockInternetConnectionChecker mockInternetConnectionChecker;
+
+  setUp(() {
+    mockInternetConnectionChecker = MockInternetConnectionChecker();
+    networkInfo = NetworkInfoImpl(mockInternetConnectionChecker);
+  });
+
+  group('isConnected', () {
+    test('should forward the call to InternetConnectionChecker.hasConnection',
+        () async {
+      // arrange
+      final tHasConnectionFuture = Future.value(true);
+
+      when(mockInternetConnectionChecker.hasConnection)
+          .thenAnswer((_) => tHasConnectionFuture);
+      // act
+      final result = networkInfo.isConnected;
+      // assert
+      verify(mockInternetConnectionChecker.hasConnection);
+      // Utilizing Dart's default referential equality.
+      // Only references to the same object are equal.
+      expect(result, tHasConnectionFuture);
+    });
+  });
+}
